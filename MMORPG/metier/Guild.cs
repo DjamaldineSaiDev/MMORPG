4378cd574fd580e4d78c7fb346a301086f75d70e
DjamaldineSaid <djamaldine.said@iut-dijon.u-bourgogne.fr>
2025-05-29 14:32:56 +0200

    midfication de la classe Guild

--- MMORPG/metier/Guild.cs
+++ MMORPG/metier/Guild.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace MMORPG.metier
 {
-   public class Guild
+    public class Guild
     {
+        private string guildId;
+        private string name;
+        private string description;
+        private int level;
+        private int maxMembers;
+        private DateTime creationDate;
+        private List<Character> members;
+
+
+        public Guild()
+        {
+
+        }
+
+        public Guild(string guildId, string name, string description, int level, int maxMembers, DateTime creationDate, List<Character> members)
+        {
+            this.guildId = guildId;
+            this.name = name;
+            this.description = description;
+            this.level = level;
+            this.maxMembers = maxMembers;
+            this.creationDate = creationDate;
+            this.members = members;
+        }
+
+        public void Invite(Character character)
+        {
+            this.members.Add(character);
+        }
+
+        public void kick(Character character)
+        {
+           
+        }
+
+        public void Promote(Character character)
+        {
+            character.LevelUp();
+        }
+
+        public void Demote(Character character)
+        {
+        }
     }
 }
